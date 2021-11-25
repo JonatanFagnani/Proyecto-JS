6a5b5e4a7b26122f8b7165d0bbc1c17de2eb6a33
Jonatan Fagnani <jlfagnani@hotmail.com>
2021-11-25 16:06:06 -0300

    Reproductor added & minor changes

--- app/app.js
+++ app/app.js
@@ -61,3 +61,30 @@ function mostrarDatos() {
       `Te has registrado correctamente ${nombre}!\nTe enviamos un mail a ${mail}. `
     );
 }
+
+//Canciones a reproducir
+
+const playCancion = document.getElementsByClassName("play");
+const stopCancion = document.getElementsByClassName("stop");
+const volumen = document.querySelector(".volumen");
+
+let audio;
+
+for (elemento of playCancion) {
+  elemento.addEventListener("click", function () {
+    let cancion = this.getAttribute("id");
+    audio = new Audio(`../assets/canciones/${cancion}.mp3`);
+    audio.play();
+  });
+}
+
+for (elemento of stopCancion) {
+  elemento.addEventListener("click", function () {
+    audio.pause();
+  });
+}
+
+volumen.addEventListener("click", function () {
+  let vol = this.value;
+  audio.volume = vol;
+});
